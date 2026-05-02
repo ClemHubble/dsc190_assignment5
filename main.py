21a0597fcf7d813ea925ecbbd677162a2432db68
Fong Clement Vo <clementsolar9@gmail.com>
2026-05-02 12:46:23 -0700

    Fix ruff lint errors

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import os
+def main():
+    print("Hello world")
 
-x = 1
-x = 2
 
-def foo():
-	y = 10
-	return y
-
-print( "hellow world" )
-
-def unused_function():
-	pass
-
-unused_variable = 123
-
-if(True):print("bad formatting")
+if __name__ == "__main__":
+    main()
